e2d1e097c3d5b5becd205598686f456089906509
Lauren Sammons <>
2018-08-22 18:27:40 -0500

    add app bar and header styling

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -127,6 +127,7 @@ export function Home() {
           </Grid>
           <Grid item xs={6}>
             <iframe
+              title="map"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3445.7100365596502!2d-97.74992788487947!3d30.273841981799404!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8644b50c99e10491%3A0x7f533588796bd16a!2s1003+Rio+Grande+St%2C+Austin%2C+TX+78701!5e0!3m2!1sen!2sus!4v1533601014545"
               width="600" height="450" frameBorder="0" allowFullScreen></iframe>
           </Grid>
